5472e1709a0a1e190d5c6d5a11662b678611d568
jufa2401 <jufa2401@gmail.com>
2018-12-30 11:10:00 +0100

    examples of map, filter and reduce

--- C-sharp/Program.cs
+++ C-sharp/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
-
-namespace C_sharp
+using System.Linq;
+using System.Collections.Generic;
+namespace learningC_sharp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> array = new List<int> { 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            // reduce
+            int sum = array.Aggregate((x, y) => x + y);
+            Console.WriteLine(sum);
+            // map
+            List<int> squaredArray = array.Select(x => x * x).ToList();
+            squaredArray.ForEach(Console.WriteLine);
+            // filter
+            List<int> wholeNumbers = array.Where(number => number % 2 == 0).ToList();
+            wholeNumbers.ForEach(Console.WriteLine);
+
         }
     }
 }
